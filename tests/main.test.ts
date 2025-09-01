fc564c01fc5fc575550a55cf4907d8fa750575f6
ejcohe22 <erikjkcohen@gmail.com>
2025-09-01 05:13:41 -0500

    make uppercase

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from "vitest";
 
-import "../src/main";
-import { initApp } from "../src/app";
+import "../src/Main";
+import { initApp } from "../src/App";
 
 describe("Three.js scene App", () => {
   it("can initialize without errors", () => {
